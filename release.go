3373ce538e22ac4d8c49bb29b7102f28d1aaacee
Chris Hicks <chicks@flow.io>
2016-07-13 14:04:47 -0400

    remove npm version change from release

--- release.go
+++ release.go
@@ -9,7 +9,6 @@ func main() {
 	executor = executor.Add("git checkout master")
 	executor = executor.Add("git pull origin master")
 	executor = executor.Add("dev tag --label micro")
-	executor = executor.Add("npm version --no-git-tag-version `sem-info tag latest`")
 	executor = executor.Add("git add package.json")
 	executor = executor.Add("git commit -m 'autocommit: package.json'")
 	executor = executor.Add("git push origin master")
