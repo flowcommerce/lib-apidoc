532a923c6f3a1123201c8b21cf6f2af01410069c
Chris Hicks <chicks@flow.io>
2016-07-13 14:05:23 -0400

    Also remove autocommit from npm version

--- release.go
+++ release.go
@@ -10,7 +10,6 @@ func main() {
 	executor = executor.Add("git pull origin master")
 	executor = executor.Add("dev tag --label micro")
 	executor = executor.Add("git add package.json")
-	executor = executor.Add("git commit -m 'autocommit: package.json'")
 	executor = executor.Add("git push origin master")
 	executor = executor.Add("npm publish")
 	executor.Run()
