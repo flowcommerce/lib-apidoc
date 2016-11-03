adea4dc1a01398393ee307bbc620e71e639d7e81
Chris Hicks <chrhicks@gmail.com>
2016-11-03 17:09:18 -0400

    Remove docgen (it was migrated to docs project (#43)
    * Remove docgen (it was migrated to docs project
    
    * update node version in travis

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
 import codegen from './codegen';
-import docgen from './docgen';
-import docparse from './docgen/docparse';
 
-export default { codegen, docgen, docparse };
+export default { codegen };
