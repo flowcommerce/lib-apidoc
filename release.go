f40874ec6eb5ff625c5086107d7b855a3267cda5
paololim <paolo@flow.io>
2021-03-23 16:16:20 -0400

    Switching master to main

--- release.go
+++ release.go
@@ -6,11 +6,11 @@ import (
 
 func main() {
 	executor := executor.Create("lib-apidoc")
-	executor = executor.Add("git checkout master")
-	executor = executor.Add("git pull origin master")
+	executor = executor.Add("git checkout main")
+	executor = executor.Add("git pull origin main")
 	executor = executor.Add("dev tag --label micro")
 	executor = executor.Add("git add package.json")
-	executor = executor.Add("git push origin master")
+	executor = executor.Add("git push origin main")
 	executor = executor.Add("npm publish")
 	executor.Run()
 }
